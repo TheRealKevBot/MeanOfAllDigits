3a61d031527160c54f9082dd851e5240a9d81cb1
TheRealKevBot <Kglas001@gmail.com>
2021-07-30 20:32:42 -0600

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+function mean(num) {
+	
+}
+
+console.log(mean(666), 6)
+console.log(mean(80), 4)
+console.log(mean(789), 8)
+console.log(mean(417), 4)
+console.log(mean(1357), 4)
+console.log(mean(42), 3)
+console.log(mean(12345), 3)
